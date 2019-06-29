80923019db9f363b36aa0d277566e4ac46fef855
Kumar Abhirup <kumar@iqubex.com>
2019-06-29 14:42:52 +0530

    👌 IMPROVE: Gulp compile CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const sourcemaps = require('gulp-sourcemaps')
 
 sass.compiler = require('node-sass')
 
 const scssTask = () =>
   gulp
     .src('./src/public/assets/styles/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./src/public/assets/styles/compiled'))
 
 const watch = () => {
