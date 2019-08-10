2e913f8fc53971f72b328f8baffb01645929b376
Kumar Abhirup <kumar@iqubex.com>
2019-08-10 11:55:05 +0530

    👌 IMPROVE: Use port 3001

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const PORT = process.env.PORT || 3006
+const PORT = process.env.PORT || 3001
 
 app
   .prepare()
