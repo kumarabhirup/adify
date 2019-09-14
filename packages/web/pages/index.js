d60e3f4a7955807a6dac9fe724d424eaa5839e23
Kumar Abhirup <kumar@iqubex.com>
2019-09-14 20:11:18 +0530

    🤣 TRY: try

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -19,7 +19,7 @@ export default function HomePage() {
         >
           ZEIT Now
         </a>
-        {process.env.TEST}
+        ENV: {process.env.TEST}
         <Link href="/post/[pid]" as="/post/test">
           <a>Test post</a>
         </Link>
