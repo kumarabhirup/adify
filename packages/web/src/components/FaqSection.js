f5e905a7ef41db06fb09c6cdf5762ef2c730273a
Kumar Abhirup <kumar@iqubex.com>
2019-09-21 15:11:38 +0530

    New Landing Page Next.js Monorepo setup! (#4)
    Thanks @pantharshit00

---
+++ packages/web/src/components/FaqSection.js
@@ -0,0 +1,54 @@
+import { faqSection } from '../api/meta'
+
+export default function FaqSection() {
+  return (
+    <section
+      id="faqSection"
+      className="accordion_part padding_bottom padding_top"
+    >
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            <div className="section_tittle text-center">
+              <h2>{faqSection.title}</h2>
+              <p>{faqSection.excerpt}</p>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div id="accordion">
+              {faqSection.questions.map(({ question, answer }, i) => (
+                <div className="card" key={i}>
+                  <div className="card-header" id="headingOne">
+                    <h5 className="mb-0">
+                      <button
+                        type="button"
+                        className="btn btn-link"
+                        data-toggle="collapse"
+                        data-target={`#collapse${i}`}
+                        aria-expanded="true"
+                        aria-controls={`collapse${i}`}
+                      >
+                        {question}
+                      </button>
+                    </h5>
+                  </div>
+
+                  <div
+                    id={`collapse${i}`}
+                    className="collapse"
+                    aria-labelledby="headingOne"
+                    data-parent="#accordion"
+                  >
+                    <div className="card-body">{answer}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ packages/web/src/components/FeaturesInDetailSection.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { featuresInDetailSection } from '../api/meta'
+import useIsMobile from './hooks/useIsMobile'
+
+/**
+ * @name FeaturesInDetailSection
+ *
+ * @description The logic written below is flawed and only works to show 2 features.
+ *              Update appreiciated.
+ */
+export default function FeaturesInDetailSection() {
+  const isMobile = useIsMobile()
+
+  return (
+    <>
+      {featuresInDetailSection.map(
+        ({ iconImage, image, embed, title, excerpt, button }, i) => {
+          const sectionClass = i % 2 === 0 ? `section_padding` : `right_time`
+          const padding =
+            featuresInDetailSection.length - 1 === i && `padding_bottom`
+
+          return (
+            <section className={`about_us ${sectionClass} ${padding}`} key={i}>
+              <div className="container">
+                <div className="row align-items-center justify-content-between">
+                  {!isMobile && sectionClass === 'section_padding' && (
+                    <div className="col-md-6 col-lg-6">
+                      <div className="learning_img">
+                        {image && <img src={image} alt="" />}
+                        {embed && embed}
+                      </div>
+                    </div>
+                  )}
+
+                  <div className="col-md-6 col-lg-5">
+                    <div className="about_us_text">
+                      <img src={iconImage} alt="" />
+                      <h2>{title}</h2>
+                      <p>{excerpt}</p>
+                      {button && (
+                        <Link href={button.href}>
+                          <a className="btn_2">{button.linkName}</a>
+                        </Link>
+                      )}
+                    </div>
+                  </div>
+
+                  {!isMobile && sectionClass === 'right_time' && (
+                    <div className="col-md-6 col-lg-6">
+                      <div className="learning_img">
+                        {image && <img src={image} alt="" />}
+                        {embed && embed}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <img
+                src="https://i.ibb.co/2dkP5zj/Ellipse-4.png"
+                alt=""
+                className="feature_icon_1 custom-animation1"
+              />
+            </section>
+          )
+        }
+      )}
+    </>
+  )
+}
