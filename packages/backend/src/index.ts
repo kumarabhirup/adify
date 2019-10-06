e3d0e3f44dd4cc0c3a0b19e9fcc3e9373a57b48f
Kumar Abhirup <kumar@iqubex.com>
2019-10-06 15:04:09 +0530

    📦 NEW: Integrate the backend!

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,28 @@
+import { GraphQLServer } from 'graphql-yoga'
+import * as cookieParser from 'cookie-parser'
+import * as bodyParser from 'body-parser'
+import * as jwt from 'jsonwebtoken'
+import * as cors from 'cors'
+
+// temporary
+import * as express from 'express'
+
+require('dotenv').config()
+
+const server = express()
+server.use(cookieParser())
+server.use(bodyParser.json({ limit: '50mb' }))
+server.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  })
+)
+
+// Routes
+server.get('/', (req, res) => {
+  res.send('<h1>Hooray!</h1>')
+})
+
+server.listen(process.env.PORT || 3006)
