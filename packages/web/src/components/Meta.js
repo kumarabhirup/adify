60ae50a7f5cefb1aebb17a55f8659034f9acf068
Kumar Abhirup <kumar@iqubex.com>
2019-10-20 12:55:29 +0530

    📦 NEW: Write Meta, OG, Twitter card tags

--- packages/web/src/components/Meta.js
+++ packages/web/src/components/Meta.js
@@ -41,7 +41,31 @@ class Meta extends Component {
           href="/static/assets/styles/compiled/style.css"
         />
 
-        <title>{meta.meta_ogTitle}</title>
+        <title>{meta.title}</title>
+
+        <meta name="description" content={meta.meta_description} />
+        <meta name="copyright" content={meta.name} />
+        <meta name="robots" content="index,follow" />
+        <meta name="author" content={`${meta.name}, ${meta.email}`} />
+        <meta name="url" content={meta.domain} />
+        <meta name="identifier-URL" content={meta.domain} />
+        <meta name="coverage" content="Worldwide" />
+        <meta name="distribution" content="Global" />
+        <meta name="rating" content="General" />
+
+        <meta property="og:title" content={meta.meta_ogTitle} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={meta.domain} />
+        <meta property="og:image" content={meta.coverImage} />
+        <meta name="fb:page_id" content={meta.fbPageId} />
+
+        <meta name="twitter:title" content={meta.meta_ogTitle} />
+        <meta name="twitter:description" content={meta.meta_description} />
+        <meta name="twitter:image" content={meta.coverImage} />
+        <meta name="twitter:image:alt" content={meta.meta_ogTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={meta.social} />
+        <meta name="twitter:creator" content={meta.social} />
       </Head>
     )
   }
