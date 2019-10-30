cf3e233f63bbd62433234cc067c3249c0d395fb3
Kumar Abhirup <kumar@iqubex.com>
2019-10-30 20:54:38 +0530

    😅 TRY: Write redux-apollo init and withData HOC
    This has errors. Not yet ready.

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import { ApolloProvider } from 'react-apollo'
 
 import Page from '../src/components/Page'
+import withData from '../src/lib/withData'
 
 class Wrapper extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -16,17 +18,19 @@ class Wrapper extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, apolloClient, pageProps } = this.props
 
     return (
       <Container>
-        <Page>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...pageProps} />
-        </Page>
+        <ApolloProvider client={apolloClient}>
+          <Page>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default Wrapper
+export default withData(Wrapper)
