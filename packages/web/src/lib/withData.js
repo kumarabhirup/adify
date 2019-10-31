0abe7a7c25f58694a9aaf3da1459d4f29e10a518
Kumar Abhirup <kumar@iqubex.com>
2019-10-31 18:57:32 +0530

    🐛 FIX: Set up true SSR app
    The app now renders the backend on the server side

--- packages/web/src/lib/withData.js
+++ packages/web/src/lib/withData.js
@@ -1,94 +1,77 @@
-/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 
 import React from 'react'
-import PropTypes from 'prop-types'
-import { ApolloProvider, getDataFromTree } from 'react-apollo'
 import Head from 'next/head'
+import { ApolloProvider, getDataFromTree } from 'react-apollo'
 
 import initApollo from './initApollo'
-import initRedux from './initRedux'
 
-// Gets the display name of a JSX component for dev tools
-function getComponentDisplayName(Component) {
-  return Component.displayName || Component.name || 'Unknown'
-}
-
-export default ComposedComponent =>
-  class WithData extends React.Component {
-    static displayName = `WithData(${getComponentDisplayName(
-      ComposedComponent
-    )})`
-
-    static propTypes = {
-      serverState: PropTypes.object.isRequired,
-    }
+export default App =>
+  class Apollo extends React.Component {
+    static displayName = 'withApollo(App)'
 
     static async getInitialProps(ctx) {
-      let serverState = {}
+      const { Component, router } = ctx
 
-      // Evaluate the composed component's getInitialProps()
-      let composedInitialProps = {}
-      if (ComposedComponent.getInitialProps) {
-        composedInitialProps = await ComposedComponent.getInitialProps(ctx)
+      let appProps = {}
+      if (App.getInitialProps) {
+        appProps = await App.getInitialProps(ctx)
       }
+
+      const apolloState = {}
+      const apollo = !process.browser
+        ? initApollo(null, ctx.ctx.req.headers.cookie)
+        : initApollo()
 
       // Run all GraphQL queries in the component tree
       // and extract the resulting data
-      if (!process.browser) {
-        const apollo = initApollo()
-        const redux = initRedux(apollo)
-        // Provide the `url` prop data in case a GraphQL query uses it
-        const url = { query: ctx.query, pathname: ctx.pathname }
+      try {
+        // Run all GraphQL queries
+        await getDataFromTree(
+          <ApolloProvider client={apollo}>
+            <App
+              {...appProps}
+              Component={Component}
+              router={router}
+              apolloState={apolloState}
+              apolloClient={apollo}
+            />
+          </ApolloProvider>
+        )
+      } catch (error) {
+        // Prevent Apollo Client GraphQL errors from crashing SSR.
+        // Handle them in components via the data.error prop:
+        // http://dev.apollodata.com/react/api-queries.html#graphql-query-data-error
+        console.error('Error while running `getDataFromTree`', error)
+      }
 
-        try {
-          // Run all GraphQL queries
-          await getDataFromTree(
-            // No need to use the Redux Provider
-            // because Apollo sets up the store for us
-            <ApolloProvider client={apollo} store={redux}>
-              <ComposedComponent url={url} {...composedInitialProps} />
-            </ApolloProvider>
-          )
-        } catch (error) {
-          // Prevent Apollo Client GraphQL errors from crashing SSR.
-          // Handle them in components via the data.error prop:
-          // http://dev.apollodata.com/react/api-queries.html#graphql-query-data-error
-        }
+      if (!process.browser) {
         // getDataFromTree does not call componentWillUnmount
         // head side effect therefore need to be cleared manually
         Head.rewind()
-
-        // Extract query data from the store
-        const state = redux.getState()
-
-        // No need to include other initial Redux state because when it
-        // initialises on the client-side it'll create it again anyway
-        serverState = {
-          apollo: {
-            // Only include the Apollo data state
-            // data: state.apollo.data,
-          },
-        }
       }
 
+      // Extract query data from the Apollo store
+      apolloState.data = apollo.cache.extract()
+
       return {
-        serverState,
-        ...composedInitialProps,
+        ...appProps,
+        apolloState,
       }
     }
 
     constructor(props) {
       super(props)
-      this.apollo = initApollo()
-      this.redux = initRedux(this.apollo, this.props.serverState)
+      // `getDataFromTree` renders the component first, the client is passed off as a property.
+      // After that rendering is done using Next's normal rendering pipeline
+      this.apolloClient =
+        props.apolloClient || initApollo(props.apolloState.data)
     }
 
     render() {
       return (
-        // No need to use the Redux Provider
-        // because Apollo sets up the store for us
-        <ApolloProvider client={this.apollo} store={this.redux}>
-          <ComposedComponent {...this.props} />
+        <ApolloProvider client={this.apolloClient}>
+          <App {...this.props} apolloClient={this.apolloClient} />
         </ApolloProvider>
       )
     }
